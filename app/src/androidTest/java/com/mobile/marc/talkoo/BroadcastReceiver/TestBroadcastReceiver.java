ee5fc87005738c919522708724cc5a247213549d
Jordy Domingos <Dyjor@Jordys-MacBook-Pro.local>
2014-12-15 04:18:53 +0800

    Broadcast Test

--- app/src/androidTest/java/com/mobile/marc/talkoo/BroadcastReceiver/TestBroadcastReceiver.java
+++ app/src/androidTest/java/com/mobile/marc/talkoo/BroadcastReceiver/TestBroadcastReceiver.java
@@ -16,28 +16,69 @@ import android.util.Log;
 
 public class TestBroadcastReceiver extends InstrumentationTestCase {
 
+    private WifiDirectBroadcastReceiver receiver_;
+    private MockWifiDirectBroadcastListener listener_;
+    private MyMockContext context_;
+    private Intent intent_;
 
     @Override
     protected void setUp() throws Exception {
+        context_ = new MyMockContext();
+        listener_ = new MockWifiDirectBroadcastListener();
+        receiver_ = new WifiDirectBroadcastReceiver();
+        receiver_.listener(listener_);
     }
 
+    public void testOnReceiveWifiEnable() {
+        intent_ = new Intent(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intent_.putExtra(WifiP2pManager.EXTRA_WIFI_STATE, WifiP2pManager.WIFI_P2P_STATE_ENABLED);
+        receiver_.onReceive(context_, intent_);
+        assertTrue(listener_.getOnIsWifiEnable());
+    }
 
-
+    public class MockIntent extends Intent {
+        public int getIntExtra(String name, int defaultValue) {
+            return WifiP2pManager.WIFI_P2P_STATE_ENABLED;
+        }
+    }
 
     private class MockWifiDirectBroadcastListener implements WifiDirectBroadcastReceiver.WifiDirectBroadcastListener {
+
+        private boolean onIsWifiEnabled_ = false;
+        private boolean onDeviceConnectedToPeers_ = false;
+        private boolean onDisconnected_ = false;
+        private boolean onPeersChangedAction_ = false;
+
+        public boolean getOnIsWifiEnable() {
+            return onIsWifiEnabled_;
+        }
+
+        public boolean getOnDeviceConnectedToPeers() {
+            return onDeviceConnectedToPeers_;
+        }
+
+        public boolean getOnDisconnected() {
+            return onDisconnected_;
+        }
+
+        public boolean getOPeersChangedAction() {
+            return onPeersChangedAction_;
+        }
+
         public void onIsWifiEnabled(boolean wifi) {
+            onIsWifiEnabled_ = true;
         }
 
         public void onDeviceConnectedToPeers() {
-
+            onDeviceConnectedToPeers_ = true;
         }
 
         public void onDisconnected() {
-
+            onDisconnected_ = true;
         }
 
         public void onPeersChangedAction() {
-
+            onPeersChangedAction_ = true;
         }
     }
 
